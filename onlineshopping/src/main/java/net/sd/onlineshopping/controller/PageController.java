6e97adbae1b97a051b1db54f18a4e90c2ee86629
Sagar Dekate <sagardekate4@gmail.com>
2019-12-05 00:49:09 -0600

    Successfully loaded static resourses again

--- onlineshopping/src/main/java/net/sd/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/sd/onlineshopping/controller/PageController.java
@@ -1,7 +1,6 @@
 package net.sd.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,26 +9,28 @@ public class PageController {
 
 	@RequestMapping(value = { "/", "/home", "/index" })
 	public ModelAndView index() {
-
+		System.out.print("Hi");
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Welcome to Spring Web MVC");
+		mv.addObject("title", "Home");
+		mv.addObject("userCLickHome", true);
 		return mv;
 	}
 
-	/*
-	 * @RequestMapping(value="/test") public ModelAndView
-	 * test(@RequestParam(value="greeting",required=false)String greeting) {
-	 * if(greeting == null) { greeting = ("Hello there"); } ModelAndView mv = new
-	 * ModelAndView("page"); mv.addObject("greeting", greeting); return mv; }
-	 */
-	
-	@RequestMapping(value="/test/{greeting}")
-	public ModelAndView test(@PathVariable("greeting")String greeting) {
-		if(greeting == null) {
-			greeting = ("Hello there");
-		}
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {
+
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", greeting);
+		mv.addObject("title", "About Us");
+		mv.addObject("userCLickAbout", true);
+		return mv;
+	}
+
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userCLickContact", true);
 		return mv;
 	}
 }
